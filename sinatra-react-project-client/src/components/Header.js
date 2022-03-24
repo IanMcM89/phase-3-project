e42a36d5b328a818bcd8a1bb4684b6b44f041efa
Ian <ianmcm.89@gmail.com>
2022-03-24 17:48:04 -0400

    added Header.js and Main.js to components folder

---
+++ sinatra-react-project-client/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../css/Header.css";
+
+function Header() {
+    return (
+      <div id="app-header">
+        <div className="header-title">
+            <h1>Recipe Book</h1>
+        </div>
+      </div>
+    );
+  }
+
+export default Header;
---
+++ sinatra-react-project-client/src/components/Main.js
@@ -0,0 +1,12 @@
+import React from "react";
+import '../css/App.css';
+
+function Main() {
+  return (
+    <div id="app-main">
+
+    </div>
+  );
+}
+
+export default Main;
