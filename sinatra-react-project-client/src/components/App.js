a0574b77c7d827277bf0909d0c2133fbe6ea38ba
Ian <ianmcm.89@gmail.com>
2022-03-25 03:28:11 -0400

    import useEffect/usestate into App.js to make fetch request to '/recipes' route

--- sinatra-react-project-client/src/components/App.js
+++ sinatra-react-project-client/src/components/App.js
@@ -1,9 +1,23 @@
+import React, { useEffect, useState } from "react";
 import Header from './Header';
 import Main from './Main';
 import { Route, Switch } from 'react-router-dom';
 import '../css/App.css';
 
 function App() {
+  const [recipes, setRecipes] = useState([]);
+  const [filteredRecipes, setFilteredRecipes] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:9292/recipes")
+    .then(r => r.json())
+    .then(recipeData => {
+      setRecipes(recipeData);
+      setFilteredRecipes(recipeData);
+      console.log(recipes)
+    })
+  },[]);
+
   return (
     <div id="app">
       <Header/>
