328e0425e844a3148031a7902e1cdd08aff2da71
Ian <ianmcm.89@gmail.com>
2022-03-26 06:16:11 -0400

    added placeholder text into Home window description

--- sinatra-react-project-client/src/components/Home.js
+++ sinatra-react-project-client/src/components/Home.js
@@ -6,9 +6,13 @@ function Home() {
     <main className="app-main">
       <div id="home-div">
         <section id="text-container">
-          <div>
+          <div id="text-container-header">
             <img src="./images/icons/logo.png" alt="Leaf Icon"/>
             <h1>Recipes.</h1>
+          </div>
+          <div id="text-container-description">
+            <h2>Ut enim ad minim veniam</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit.</p>
           </div>
         </section>
         <section id="image-container"/>
