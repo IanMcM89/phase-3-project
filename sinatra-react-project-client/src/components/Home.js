5bb04b39da27a6f0533acc7dc8d08e17456e7d25
Ian <ianmcm.89@gmail.com>
2022-03-27 05:31:58 -0400

    added animated css to Home page

--- sinatra-react-project-client/src/components/Home.js
+++ sinatra-react-project-client/src/components/Home.js
@@ -4,22 +4,28 @@ import '../css/Home.css';
 function Home() {
   return (
     <main className="app-main">
-      <div id="home-div">
-        <div id="text-container">
-          <section id="text-container-header">
-            <img src="./images/icons/logo.png" alt="Leaf Icon"/>
-            <h1>NNR.</h1>
-          </section>
-          <section id="text-container-description">
-            <h2>No Nonsense. All Recipes.</h2>
-            <p>Tired of history lessons and endless scrolling every time you search for a recipe online? NNR provides only recipes and ingredients, without all the...extra...information. Simply choose a category to get started or browse through the recipes catalogue.</p>
-          </section>
-          <section id="text-container-button">
-            <a href="/categories" class="button">Get Started</a>
-          </section>
+      <div class='container'>
+        <div class="container_content">
+          <div class="container_content_inner">
+            <div class="title">
+              <img src="./images/icons/logo.png" alt="Leaf Icon"/>
+              <h1>NNR.</h1>
+            </div>
+            <div class="par">
+              <h2>No Nonsense. All Recipes.</h2>
+              <p>
+                Tired of history lessons and endless scrolling every time you search for a recipe online? NNR provides only recipes and ingredients, without all the...extra...information. Simply choose a category to get started or browse through the recipes catalogue.
+              </p>
+            </div>
+            <div class="btns">
+              <a href="/categories" class="button">Get Started</a>
+            </div>
+          </div>
         </div>
-        <figure id="image-container"/>
+        <div class="container_img">
+        </div>
       </div>
+      <div class="overlay"></div>
     </main>
   );
 }
