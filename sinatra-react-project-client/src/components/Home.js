8d5e4a5fd8cb8c474e100fc19b72ee0f5a4a2d54
Ian <ianmcm.89@gmail.com>
2022-03-28 08:00:14 -0400

    change 'overlay' class to 'overlay-left'

--- sinatra-react-project-client/src/components/Home.js
+++ sinatra-react-project-client/src/components/Home.js
@@ -24,7 +24,7 @@ function Home() {
         </div>
         <div className="container_img"/>
       </div>
-      <div class="overlay"></div>
+      <div class="overlay-left"></div>
     </main>
   );
 }
