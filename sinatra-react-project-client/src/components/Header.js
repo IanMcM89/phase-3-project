37aeb8c8c5444f1e3ae3df6d10c145b5b5cb529f
Ian <ianmcm.89@gmail.com>
2022-03-30 03:03:13 -0400

    consolidated Dashboard code into Header component and deleted Dashboard.js

--- sinatra-react-project-client/src/components/Header.js
+++ sinatra-react-project-client/src/components/Header.js
@@ -1,13 +1,41 @@
 import React from "react";
-import Dashboard from "./Dashboard";
+import { useHistory } from 'react-router-dom';
 import "../css/Header.css";
 
 function Header({ resetRecipes }) {
-    return (
-      <div id="app-header">
-        <Dashboard resetRecipes={resetRecipes}/>
-      </div>
-    );
+  const history = useHistory();
+
+  //Redirects route path based on case provided by tab text content:
+  function handleClick(e) {
+      
+    resetRecipes();
+    
+    switch (e.target.textContent) {
+        case 'Create':
+            history.push("/create")
+            break;
+        case 'Recipes':
+            history.push("/recipes");
+            break;
+        case 'Categories':
+            history.push("/categories")
+            break;
+        default:
+            return history.push("/");
+    }       
   }
+
+  return (
+    <div id="app-header">
+      <div id="header-buttons">
+        {/* Dashboard buttons */}
+        <button onClick={handleClick}>Home</button>
+        <button onClick={handleClick}>Categories</button>
+        <button onClick={handleClick}>Recipes</button>
+        <button onClick={handleClick}>Create</button>
+      </div>
+    </div>
+  );
+}
 
 export default Header;
