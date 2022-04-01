81fc79e780d79f9ecfd07b3dc088b1a5d35692e4
Ian <ianmcm.89@gmail.com>
2022-04-01 03:45:36 -0400

    changed 'props' to '{ resetRecipes }' in Header component

--- sinatra-react-project-client/src/components/Header.js
+++ sinatra-react-project-client/src/components/Header.js
@@ -2,13 +2,13 @@ import React from "react";
 import { useHistory } from 'react-router-dom';
 import "../css/Header.css";
 
-function Header(props) {
+function Header({ resetRecipes }) {
   const history = useHistory();
 
   //On button click, redirect path, toggle search bar and reset recipes:
   function handleClick(e) {
     history.push(`/${(e.target.textContent).toLowerCase()}`);
-    props.resetRecipes();
+    resetRecipes();
   }
 
   return (
