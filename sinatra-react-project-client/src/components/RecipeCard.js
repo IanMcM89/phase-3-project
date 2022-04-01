a2e9bc29807674c4f2a49823d29ee2afa90b67d9
Ian <ianmcm.89@gmail.com>
2022-04-01 03:46:59 -0400

    added click function to recipe-card div to redirect to 'recipe/id' on click

--- sinatra-react-project-client/src/components/RecipeCard.js
+++ sinatra-react-project-client/src/components/RecipeCard.js
@@ -1,9 +1,16 @@
 import React from "react";
+import { useHistory } from 'react-router-dom';
 import '../css/Recipe.css';
 
 function RecipeCard({ recipe }) {
+  const history = useHistory();
+
+  const handleClick = () => {
+    history.push(`/recipes/${recipe.id}`);
+  }
+
   return (
-    <div className="recipe-card">
+    <div className="recipe-card" onClick={handleClick}>
       <div className="recipe-card-content" style={{backgroundImage: `url(${recipe.image_url})`}}>
         <p className="recipe-card-p">{recipe.title}</p>
       </div>
