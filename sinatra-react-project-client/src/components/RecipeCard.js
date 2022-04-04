7a5b37c3271beeac744058525301ede7d405e2cf
Ian <ianmcm.89@gmail.com>
2022-04-04 04:44:16 -0400

    changed Recipe.css to RecipePage.css

--- sinatra-react-project-client/src/components/RecipeCard.js
+++ sinatra-react-project-client/src/components/RecipeCard.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useHistory } from 'react-router-dom';
-import '../css/Recipe.css';
+import '../css/RecipesPage.css';
 
 function RecipeCard({ recipe }) {
   const history = useHistory();
